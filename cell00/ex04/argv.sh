294182661c3e1e2bab0ad7db919fd162306d21ed
quibble <phu.sirirat@gmail.com>
2026-02-03 00:13:11 +0700

    Done ex04

---
+++ cell00/ex04/argv.sh
@@ -0,0 +1,11 @@
+if [ $# -eq 0 ]; then
+    echo "No arguments supplied"
+else
+    echo "$1"
+    if [ -n "$2" ]; then
+        echo "$2"
+    fi
+    if [ -n "$3" ]; then
+        echo "$3"
+    fi
+fi
